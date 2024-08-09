fcc1677cf76dc83a992fb43580d3bd74c2160341
ffg <58115022@qq.com>
2024-08-09 11:12:03 +0800

    '按照业务逻辑拆分函数'

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,38 +1,12 @@
 <script setup>
-  import { getCategoryAPI } from '@/apis/category.js';
-  import { onMounted, ref } from 'vue';
-  import { useRoute } from 'vue-router';
-  import { getBannerAPI } from '@/apis/home';
   import GoodsItem from '../Home/components/GoodsItem.vue';
-  import { onBeforeRouteUpdate } from 'vue-router';
-  //获取数据
-  const categoryData = ref([])
-  const route = useRoute()
-  const getCategory = async (id = route.params.id)=>{
-    const res = await getCategoryAPI(id)
-    categoryData.value = res.result
-  }
-
-  onMounted(() => getCategory())
-
-  //目标：路由参数变化的时候，可以把分类数据接口重新发送
-  onBeforeRouteUpdate((to)=>{
-    //解决问题:使用最新的路由参数请求最新的分类数据
-    getCategory(to.params.id)
-    
-  })
-
+  import { useBanner } from './composables/useBanner';
+  import { useCategory } from './composables/useCategory';
   //获取banner
-const bannerList = ref([])
-
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite:'2'
-  })
-  bannerList.value = res.result
-}
-
-onMounted(() => getBanner())
+  const { bannerList} = useBanner()
+  
+  //获取分类数据
+  const { categoryData } = useCategory()
 </script>
 
 <template>
