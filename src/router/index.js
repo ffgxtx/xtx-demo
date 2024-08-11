3e7de2700fa092da1116e2d31db1bc844a92f4a7
ffg <58115022@qq.com>
2024-08-12 01:55:22 +0800

    '二级分类-路由滚动行为体验优化'

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,13 @@ const router = createRouter({
       path:'/Login',
       component:Login
     }
-  ]
+  ],
+  //路由滚动行为定制
+  scrollBehavior(){
+    return{
+      top:0
+    }
+  }
 })
 
 export default router
