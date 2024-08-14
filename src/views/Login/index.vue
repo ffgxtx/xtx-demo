a52e2c0e36e681000b77fe12b9b07d77b974789b
ffg <58115022@qq.com>
2024-08-14 10:11:06 +0800

    '自定义校验规则'

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -6,7 +6,8 @@ import { ref } from 'vue';
 //准备表单对象
 const form = ref({
   account:'',
-  password:''
+  password:'',
+  agree:true
 })
 
 //准备规则对象
@@ -27,6 +28,20 @@ const rules = {
       max:14,
       message:'密码长度为6-14个字符',
       trigger:'blur'
+    }
+  ],
+  agree:[
+    {
+      validator:(rule,value,callback) =>{
+        //自定义校验逻辑
+        //勾选就通过 不勾选不通过
+        if(value){
+          callback()
+        }else{
+          callback(new Error('请勾选协议'))
+        }
+        
+      }
     }
   ]
 }
@@ -62,8 +77,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
