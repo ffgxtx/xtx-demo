7c6ceefca83538134a5a8eb137c91c951182c716
ffg <58115022@qq.com>
2024-08-17 00:05:37 +0800

    '结算页-提交订单业务实现'

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,8 @@ import SubCategory from '@/views/SubCategory/indexe.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
+import Pay from '@/views/Pay/index.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和component对应关系的位置
@@ -39,6 +41,9 @@ const router = createRouter({
         },{
           path:'checkout',
           component:Checkout
+        },{
+          path:'pay',
+          component:Pay
         }
 
       ]
